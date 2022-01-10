aedfbaa1010187331fc519728b1229b8153c93be
MiketoCoding <le.michael.v@gmail.com>
2022-01-10 17:49:15 -0500

    Add index.js and restaurants routes api
    Index.js file and restaurants.routes.js api file added

---
+++ index.js
@@ -0,0 +1,37 @@
+// connect to DB and start the server
+import app from './server.js'
+import mongodb from 'mongodb'
+import dotenv from 'dotenv'
+// now configure dotenv
+dotenv.config()
+// get access to mongo client
+const MongoClient = mongodb.MongoClient
+
+// to access and environment variable
+const port = process.env.PORT || 8000
+
+// access the db
+MongoClient.connect(
+  // once connect to DB need to pass in information
+  // our env variables - restreviews_db_uri
+  process.env.RESTREVIEWS_DB_URI,
+  {
+    // pass in options for accessing the db
+    // only 50 people can access at one time
+    poolSize: 50,
+    // after 2500ms request will timeout
+    wtimeout: 2500,
+    // 
+    useNewUrlParse: true 
+  }
+  )
+  .catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+  })
+  .then(async client => {
+    // how we start app server after db connected
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`)
+    })
+  })
